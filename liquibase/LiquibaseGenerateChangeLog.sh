c1bc44ef1948384b379ceb5adab169f4f873c0c7
Garve Hays <gkhays@gmail.com>
2016-07-25 17:05:15 -0500

    Use Liquibase Script
    Built-in Liquibase .bat and .sh scripts handle the invocation.

--- liquibase/LiquibaseGenerateChangeLog.sh
+++ liquibase/LiquibaseGenerateChangeLog.sh
@@ -2,13 +2,13 @@
 
 # TODO - We need some variables up in here!
 
-JAR=liquibase-core-3.4.2.jar
 DRIVER=org.postgresql.Driver
 JDBC_PROTO=jdbc:postgresql://
 JDBC_URL="${JDBC_PROTO}${HOST}:${PORT}/${DBNAME}"
 
-java  -Dfile.encoding=UTF-8 \
-    -jar ${JAR} \
+# TODO - As with the diff script, we can invoke liquibase.sh
+# see http://www.liquibase.org/documentation/command_line.html
+${LIQUIBASE_HOME}/liquibase  -Dfile.encoding=UTF-8 \
     --classpath=${CLASSPATH} \
     --driver=${DRIVER} \
     --url="${JDBCURL}" \
